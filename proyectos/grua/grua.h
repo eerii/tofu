3fc66908325962f589fb04f30ff19f8990cb5e16
José Pazos Pérez <jose.pazos.perez@rai.usc.es>
2023-03-22 12:58:34 +0100

    Grua compatible 🦋

---
+++ proyectos/grua/grua.h
@@ -0,0 +1,136 @@
+// Estructura con los datos de la grúa
+// Compatible con las versiones en 1.2 y 3.3
+#pragma once
+
+#include <array>
+#include <vector>
+#include <cstdint>
+
+using v3 = std::array<float, 3>;
+using ui32 = std::uint32_t;
+
+// Posiciones relativas de los objetos entre si
+enum PosicionamientoRelativo {
+    POS_CENTRO,
+    POS_ENCIMA,
+    POS_DEBAJO,
+    POS_IZQUIERDA,
+    POS_DERECHA,
+    POS_DELANTE,
+    POS_DETRAS,
+};
+
+// Piezas de la grúa
+struct PiezaGrua {
+    int id, padre;
+    PosicionamientoRelativo rel;
+    v3 pos_rel;
+    v3 escala;
+    v3 color;
+    float angulo;
+
+    PiezaGrua(v3 p_pos_rel, v3 p_escala, v3 p_color, int p_padre = -1, PosicionamientoRelativo p_rel = POS_CENTRO, float p_angulo = 0.f)
+        : pos_rel(p_pos_rel), escala(p_escala), color(p_color), padre(p_padre), rel(p_rel), angulo(p_angulo) {
+        static int count = 0;
+        id = count++;
+    }
+};
+
+enum PiezasImportantes {
+    PIEZA_BASE = 1,
+    PIEZA_TORRE = 4,
+    PIEZA_BRAZO = 5,
+    PIEZA_CABLE = 6
+};
+
+inline std::vector<PiezaGrua> piezas_grua = {
+    { {0.f, 0.f, 0.f}, {60.f, 1.f, 60.f}, {0.3f, 0.2f, 0.5f} }, // Suelo
+    { {0.f, -2.f, 0.f}, {3.f, 0.5f, 4.f}, {1.f, 1.f, 0.5f} }, // Base
+    { {0.f, 0.f, 0.f}, {1.f, 1.f, 4.5f}, {0.7f, 1.f, 0.9f}, PIEZA_BASE, POS_IZQUIERDA }, // Pie izquierdo de la base
+    { {0.f, 0.f, 0.f}, {1.f, 1.f, 4.5f}, {0.7f, 1.f, 0.9f}, PIEZA_BASE, POS_DERECHA }, // Pie derecho de la base
+    { {0.f, 0.f, 0.f}, {1.f, 8.f, 1.f}, {0.5f, 0.5f, 1.f}, PIEZA_BASE, POS_ENCIMA }, // Torre central
+    { {-4.f, 0.f, 0.f}, {8.f, 1.f, 1.f}, {1.f, 0.5f, 0.5f}, PIEZA_TORRE, POS_ENCIMA }, // Brazo
+    { {-7.f, 0.f, 0.f}, {0.1f, 6.f, 0.1f}, {1.f, 0.8f, 1.f}, PIEZA_BRAZO, POS_DEBAJO }, // Cable
+    { {0.f, 0.f, 0.f}, {0.5f, 0.5f, 0.5f}, {1.f, 0.9f, 0.5f}, PIEZA_CABLE, POS_DEBAJO }, // Gancho
+};
+
+// Obtiene la posición relativa de una pieza
+inline v3 posRelativa(ui32 pieza) {
+    PiezaGrua& p = piezas_grua[pieza];
+    PiezaGrua* padre = p.padre < 0 ? nullptr : &piezas_grua[p.padre];
+   
+    v3 pos = p.pos_rel;
+    switch (p.rel) {
+        case POS_CENTRO:
+            break;
+        case POS_ENCIMA:
+            pos[1] -= padre->escala[1] + p.escala[1];
+            break;
+        case POS_DEBAJO:
+            pos[1] += padre->escala[1] + p.escala[1];
+            break;
+        case POS_IZQUIERDA:
+            pos[0] -= padre->escala[0] + p.escala[0];
+            break;
+        case POS_DERECHA:
+            pos[0] += padre->escala[0] + p.escala[0];
+            break;
+        case POS_DELANTE:
+            pos[2] -= padre->escala[2] + p.escala[2];
+            break;
+        case POS_DETRAS:
+            pos[2] += padre->escala[2] + p.escala[2];
+            break;
+    }
+
+    return pos;
+}
+
+inline struct Controles {
+    bool delante, detras, girar_der, girar_izq;
+    bool torre_der, torre_izq, torre_arriba, torre_abajo;
+    bool brazo_extender, brazo_contraer;
+    bool cable_soltar, cable_recoger;
+} controles;
+
+// Controlar la grua
+void controlarGrua() {
+    // Mover base
+    if (controles.delante)
+        piezas_grua[PIEZA_BASE].pos_rel[2] -= 0.1f;
+    if (controles.detras)
+        piezas_grua[PIEZA_BASE].pos_rel[2] += 0.1f;
+
+    // Mover torre
+    if (controles.torre_der)
+        piezas_grua[PIEZA_TORRE].angulo += 0.1f;
+    if (controles.torre_izq)
+        piezas_grua[PIEZA_TORRE].angulo -= 0.1f;
+    if (controles.torre_arriba)
+        piezas_grua[PIEZA_TORRE].escala[1] = std::clamp(piezas_grua[PIEZA_TORRE].escala[1] + 0.05f, 3.f, 10.f);
+    if (controles.torre_abajo)
+        piezas_grua[PIEZA_TORRE].escala[1] = std::clamp(piezas_grua[PIEZA_TORRE].escala[1] - 0.05f, 3.f, 10.f);
+
+    // Mover brazo
+    if (controles.brazo_extender and piezas_grua[PIEZA_BRAZO].escala[0] < 12.f) {
+        piezas_grua[PIEZA_BRAZO].escala[0] += 0.1f;
+        piezas_grua[PIEZA_BRAZO].pos_rel[0] -= 0.1f;
+        piezas_grua[PIEZA_CABLE].pos_rel[0] -= 0.1f;
+    }
+    if (controles.brazo_contraer and piezas_grua[PIEZA_BRAZO].escala[0] > 6.f) {
+        piezas_grua[PIEZA_BRAZO].escala[0] -= 0.1f;
+        piezas_grua[PIEZA_BRAZO].pos_rel[0] += 0.1f;
+        piezas_grua[PIEZA_CABLE].pos_rel[0] += 0.1f;
+    }
+
+    // Mover cable
+    if (controles.cable_recoger)
+        piezas_grua[PIEZA_CABLE].escala[1] -= 0.1f;
+    if (controles.cable_soltar)
+        piezas_grua[PIEZA_CABLE].escala[1] += 0.1f;
+    if (piezas_grua[PIEZA_CABLE].escala[1] < 2.f)
+        piezas_grua[PIEZA_CABLE].escala[1] = 2.f;
+    if (piezas_grua[PIEZA_CABLE].escala[1] - piezas_grua[PIEZA_TORRE].escala[1] > 0.f)
+        piezas_grua[PIEZA_CABLE].escala[1] = piezas_grua[PIEZA_TORRE].escala[1];
+}
+
--- proyectos/grua/main.cpp
+++ proyectos/grua/main.cpp
@@ -4,6 +4,7 @@
 #include "tofu.h"
 using namespace tofu;
 
+#include "grua.h"
 #include "camara.h"
 
 #include <map>
@@ -15,35 +16,12 @@ const std::vector<ui32> atributos = { 3 /* Pos */ };
 
 // ---
 
-// Posiciones relativas de los objetos entre si
-enum PosicionamientoRelativo {
-    POS_CENTRO,
-    POS_ENCIMA,
-    POS_DEBAJO,
-    POS_IZQUIERDA,
-    POS_DERECHA,
-    POS_DELANTE,
-    POS_DETRAS,
+struct Modelo {
+    glm::mat4 trans;
+    glm::mat4 rot;
+    glm::mat4 model;
 };
-
-// Objetos a dibujar
-struct Objeto {
-    int id, padre;
-
-    PosicionamientoRelativo rel;
-    glm::vec3 pos_rel;
-    glm::mat4 trans = glm::mat4(1.0f);
-
-    glm::vec3 escala;
-    glm::mat4 esc = glm::mat4(1.0f);
-
-    float angulo = 0.f;
-    glm::mat4 rot = glm::mat4(1.0f);
-
-    glm::vec3 color;
-};
-std::vector<Objeto> objetos;
-int base, torre, brazo, cable;
+inline std::vector<Modelo> modelos_grua;
 
 // Buffers de instancias
 TexBuffer buf_modelo;
@@ -51,91 +29,39 @@ TexBuffer buf_color;
 
 // ---
 
-int nuevoObjeto(glm::vec3 pos, glm::vec3 escala, glm::vec3 color, int padre = -1, PosicionamientoRelativo rel = POS_CENTRO) {
-    int id = objetos.size();
-
-    objetos.push_back({
-        .id = id,
-        .padre = padre,
-        .rel = rel,
-        .pos_rel = pos,
-        .escala = escala,
-        .color = color
-    });
-
-    return id;
-}
-
-void eliminarObjeto(ui32 id) {
-    objetos.erase(objetos.begin() + id);
-    for (ui32 i = id; i < objetos.size(); i++)
-        objetos[i].id--;
-}
-
-glm::vec3 posRelativa(ui32 objeto) {
-    Objeto& obj = objetos[objeto];
-    Objeto* padre = obj.padre < 0 ? nullptr : &objetos[obj.padre];
-   
-    glm::vec3 pos = obj.pos_rel;
-    switch (obj.rel) {
-        case POS_CENTRO:
-            break;
-        case POS_ENCIMA:
-            pos.y -= padre->escala.y + obj.escala.y;
-            break;
-        case POS_DEBAJO:
-            pos.y += padre->escala.y + obj.escala.y;
-            break;
-        case POS_IZQUIERDA:
-            pos.x -= padre->escala.x + obj.escala.x;
-            break;
-        case POS_DERECHA:
-            pos.x += padre->escala.x + obj.escala.x;
-            break;
-        case POS_DELANTE:
-            pos.z -= padre->escala.z + obj.escala.z;
-            break;
-        case POS_DETRAS:
-            pos.z += padre->escala.z + obj.escala.z;
-            break;
-    }
-
-    return pos;
+glm::vec3 vecFromArray(v3 v) {
+    return glm::vec3(v[0], v[1], v[2]);
 }
 
 glm::mat4 modeloObjeto(ui32 id) {
     glm::mat4 modelo = glm::mat4(1.f);
 
-    Objeto& obj = objetos[id];
-    Objeto* padre = obj.padre < 0 ? nullptr : &objetos[obj.padre];
+    PiezaGrua& obj = piezas_grua[id];
+    Modelo& m_obj = modelos_grua[id];
+    PiezaGrua* padre = obj.padre < 0 ? nullptr : &piezas_grua[obj.padre];
+    Modelo* m_padre = obj.padre < 0 ? nullptr : &modelos_grua[obj.padre];
 
     std::vector<glm::mat4> modelos;
     while (padre != nullptr) {
-        modelos.push_back(padre->trans * padre->rot);
-        padre = padre->padre < 0 ? nullptr : &objetos[padre->padre];
+        modelos.push_back(m_padre->trans * m_padre->rot);
+        m_padre = padre->padre < 0 ? nullptr : &modelos_grua[padre->padre];
+        padre = padre->padre < 0 ? nullptr : &piezas_grua[padre->padre];
     }
     for (auto it = modelos.rbegin(); it != modelos.rend(); it++)
         modelo *= *it;
 
-    obj.trans = glm::translate(glm::mat4(1.f), posRelativa(id));
-    obj.rot = glm::rotate(glm::mat4(1.f), obj.angulo, glm::vec3(modelo[1].x, modelo[1].y, modelo[1].z));
-    obj.esc = glm::scale(glm::mat4(1.f), obj.escala);
-    modelo *= obj.trans * obj.rot * obj.esc;
+    m_obj.trans = glm::translate(glm::mat4(1.f), vecFromArray(posRelativa(id)));
+    m_obj.rot = glm::rotate(glm::mat4(1.f), obj.angulo, glm::vec3(modelo[1].x, modelo[1].y, modelo[1].z));
+    glm::mat4 esc = glm::scale(glm::mat4(1.f), vecFromArray(obj.escala));
+    modelo *= m_obj.trans * m_obj.rot * esc;
 
     return modelo;
 }
 
 void actualizarModelosObjetos() {
     std::vector<glm::mat4> modelos;
-    for (ui32 id = 0; id < objetos.size(); id++) {
-        if (modelos.size() == id)
-            modelos.push_back(modeloObjeto(id));
-        else if (modelos.size() > id)
-            modelos[id] = modeloObjeto(id);
-        else {
-            log::error("No se ha podido actualizar el modelo del objeto {}", id);
-            std::exit(1);
-        }
+    for (ui32 id = 0; id < piezas_grua.size(); id++) {
+        modelos.push_back(modeloObjeto(id));
     }
 
     buffer::cargar(buf_modelo.b, modelos, 0);
@@ -146,7 +72,7 @@ void actualizarModelosObjetos() {
 
 void actualizarColoresObjetos() {
     std::vector<glm::vec4> colores;
-    std::transform(objetos.begin(), objetos.end(), std::back_inserter(colores), [](Objeto &p) { return glm::vec4(p.color, 0.f); });
+    std::transform(piezas_grua.begin(), piezas_grua.end(), std::back_inserter(colores), [](PiezaGrua &p) { return glm::vec4(vecFromArray(p.color), 0.f); });
 
     buffer::cargar(buf_color.b, colores, 0);
 
@@ -156,50 +82,26 @@ void actualizarColoresObjetos() {
 
 // ---
 
-void controlarGrua() {
+void inputGrua() {
     // Mover base
-    if (gl.io.teclas[GLFW_KEY_UP].mantenida)
-        objetos[base].pos_rel.z -= 0.1f;
-    if (gl.io.teclas[GLFW_KEY_DOWN].mantenida)
-        objetos[base].pos_rel.z += 0.1f;
-    if (gl.io.teclas[GLFW_KEY_LEFT].mantenida)
-        objetos[base].pos_rel.x -= 0.1f;
-    if (gl.io.teclas[GLFW_KEY_RIGHT].mantenida)
-        objetos[base].pos_rel.x += 0.1f;
+    controles.delante = gl.io.teclas[GLFW_KEY_UP].mantenida;
+    controles.detras = gl.io.teclas[GLFW_KEY_DOWN].mantenida;
+    controles.girar_der = gl.io.teclas[GLFW_KEY_LEFT].mantenida;
+    controles.girar_izq = gl.io.teclas[GLFW_KEY_RIGHT].mantenida;
 
     // Mover torre
-    if (gl.io.teclas[GLFW_KEY_H].mantenida)
-        objetos[torre].angulo += 0.1f;
-    if (gl.io.teclas[GLFW_KEY_J].mantenida)
-        objetos[torre].angulo -= 0.1f;
-    if (gl.io.teclas[GLFW_KEY_U].mantenida)
-        objetos[torre].escala.y = std::clamp(objetos[torre].escala.y + 0.05f, 3.f, 10.f);
-    if (gl.io.teclas[GLFW_KEY_Y].mantenida)
-        objetos[torre].escala.y = std::clamp(objetos[torre].escala.y - 0.05f, 3.f, 10.f);
+    controles.torre_der = gl.io.teclas[GLFW_KEY_H].mantenida;
+    controles.torre_izq = gl.io.teclas[GLFW_KEY_J].mantenida;
+    controles.torre_arriba = gl.io.teclas[GLFW_KEY_U].mantenida;
+    controles.torre_abajo = gl.io.teclas[GLFW_KEY_Y].mantenida;
 
     // Mover brazo
-    if (gl.io.teclas[GLFW_KEY_K].mantenida and objetos[brazo].escala.x < 12.f) {
-        objetos[brazo].escala.x += 0.1f;
-        objetos[brazo].pos_rel.x -= 0.1f;
-        objetos[cable].pos_rel.x -= 0.1f;
-    }
-    if (gl.io.teclas[GLFW_KEY_L].mantenida and objetos[brazo].escala.x > 6.f) {
-        objetos[brazo].escala.x -= 0.1f;
-        objetos[brazo].pos_rel.x += 0.1f;
-        objetos[cable].pos_rel.x += 0.1f;
-    }
+    controles.brazo_extender = gl.io.teclas[GLFW_KEY_K].mantenida;
+    controles.brazo_contraer = gl.io.teclas[GLFW_KEY_L].mantenida;
 
     // Mover cable
-    if (gl.io.teclas[GLFW_KEY_I].mantenida)
-        objetos[cable].escala.y -= 0.1f;
-    if (gl.io.teclas[GLFW_KEY_O].mantenida)
-        objetos[cable].escala.y += 0.1f;
-    if (objetos[cable].escala.y < 2.f)
-        objetos[cable].escala.y = 2.f;
-    if (objetos[cable].escala.y - objetos[torre].escala.y > 0.f)
-        objetos[cable].escala.y = objetos[torre].escala.y;
-
-    actualizarModelosObjetos();
+    controles.cable_recoger = gl.io.teclas[GLFW_KEY_I].mantenida;
+    controles.cable_soltar = gl.io.teclas[GLFW_KEY_O].mantenida;
 }
 
 // ---
@@ -207,14 +109,16 @@ void controlarGrua() {
 void render() {
     // Variables
     camara();
+    inputGrua();
     controlarGrua();
+    actualizarModelosObjetos();
 
     // Shader y uniforms
     shader::usar("grua");
     shader::uniform("viewproj", gl.proj * gl.view);
     
     // Dibujar objetos por instancias
-    dibujar(objetos.size(), "cubo");
+    dibujar(piezas_grua.size(), "cubo");
 }
 
 // ---
@@ -238,53 +142,7 @@ int main() {
     std::srand(std::time(nullptr));
 
     // Crear objetos
-    nuevoObjeto( // Suelo
-        glm::vec3(0.f, 0.f, 0.f),
-        glm::vec3(60.f, 1.f, 60.f),
-        glm::vec3(0.3f, 0.2f, 0.5f)
-    );
-    base = nuevoObjeto( // Base
-        glm::vec3(0.f, -2.f, 0.f),
-        glm::vec3(3.f, 0.5f, 4.f),
-        glm::vec3(1.f, 1.f, 0.5f)
-    );
-    nuevoObjeto( // Pie izquierdo de la base
-        glm::vec3(0.f, 0.f, 0.f),
-        glm::vec3(1.f, 1.f, 4.5f),
-        glm::vec3(0.7f, 1.f, 0.9f),
-        base, POS_IZQUIERDA
-    );
-    nuevoObjeto( // Pie derecho de la base
-        glm::vec3(0.f, 0.f, 0.f),
-        glm::vec3(1.f, 1.f, 4.5f),
-        glm::vec3(0.7f, 1.f, 0.9f),
-        base, POS_DERECHA
-    );
-    torre = nuevoObjeto( // Torre central
-        glm::vec3(0.f, 0.f, 0.f),
-        glm::vec3(1.f, 8.f, 1.f),
-        glm::vec3(0.5f, 0.5f, 1.f),
-        base, POS_ENCIMA
-    );
-    brazo = nuevoObjeto( // Brazo de la grua
-        glm::vec3(-4.f, 0.f, 0.f),
-        glm::vec3(8.f, 1.f, 1.f),
-        glm::vec3(1.f, 0.5f, 0.5f),
-        torre, POS_ENCIMA
-    );
-    cable = nuevoObjeto( // Cable de la grua
-        glm::vec3(-7.f, 0.f, 0.f),
-        glm::vec3(0.1f, 6.f, 0.1f),
-        glm::vec3(1.f, 0.8f, 1.f),
-        brazo, POS_DEBAJO
-    );
-    nuevoObjeto( // Gancho de la grua
-        glm::vec3(0.f, 0.f, 0.f),
-        glm::vec3(0.5f, 0.5f, 0.5f),
-        glm::vec3(1.f, 0.9f, 0.5f),
-        cable, POS_DEBAJO
-    );
-    
+    modelos_grua.resize(piezas_grua.size());
     actualizarModelosObjetos();
     actualizarColoresObjetos();
 
